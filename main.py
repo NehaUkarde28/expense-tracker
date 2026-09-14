7ea7b525ac8e6c2a725d4d43f553c39d55ea771e
Neha Ukarde <nehaukarde83@gmail.com>
2026-09-15 03:14:10 +0530

    Store expenses in list

--- main.py
+++ main.py
@@ -7,9 +7,19 @@ print("4. Exit")
 x = input("Enter your choice: ")
 print(x)
 
+expenses =[]
+
 if x == "1":
     print("Add expense selected!")
+    expense_name = input("Enter Expense Name: ")
+    amount= float(input("Enter Amount:"))
+    category = input("Enter Category:")
+    expenses.append([expense_name,amount,category])
+    print("Expense added succesfully!")
+    
 elif x == "2":
+    for expense in expenses:
+        print(expense)
     print("View expense selected!")
 elif x == "3":
     print("Total expenses")
